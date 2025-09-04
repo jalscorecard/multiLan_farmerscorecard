7a8ea6e72110a054d3c70ca28453208bde3e321f
deevan <deevan@kali.kali>
2025-09-04 13:27:40 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://multilan-farmerscorecard.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
